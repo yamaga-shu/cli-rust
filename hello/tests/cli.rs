71625fde27262b78207652f760bdc0aa17fa53d5
shu.yamaga <s.yamaga.0318@gmail.com>
2025-12-27 12:40:16 +0900

    Uupdate test for local command

--- hello/tests/cli.rs
+++ hello/tests/cli.rs
@@ -1,9 +1,11 @@
+use assert_cmd::cargo;
+use assert_cmd::prelude::*;
+
 use std::process::Command;
 
 #[test]
 fn runs() {
-    let mut cmd = Command::new("ls");
-    let res = cmd.output();
+    let mut cmd = Command::new(cargo::cargo_bin!("hello"));
 
-    assert!(res.is_ok())
+    cmd.assert().success();
 }
