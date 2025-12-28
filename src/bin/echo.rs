36749649917177e384ee4817f5695cf684287da8
shu.yamaga <s.yamaga.0318@gmail.com>
2025-12-29 08:51:28 +0900

    Update `echo` by clap

--- src/bin/echo.rs
+++ src/bin/echo.rs
@@ -1,3 +1,9 @@
+use clap::Command;
+
 fn main() {
-    println!("{:?}", std::env::args());
+    let _m = Command::new("cli-rust")
+        .author("yamaga-shu, s.yamaga.0318@gmail.com")
+        .version("0.0.1")
+        .about("Rust echo")
+        .get_matches();
 }
